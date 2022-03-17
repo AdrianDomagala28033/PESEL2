ab8afceb047834693a4b9cc95d2dc9d6dbc8026b
Adrian Domagała <57833110+ArdianOOO@users.noreply.github.com>
2022-03-17 09:51:02 +0100

    Add files via upload

---
+++ PESEL/Pesel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramPesel
+{
+    class Pesel
+    {
+        private string numerPesel;
+        public string Plec
+        {
+            get
+            {
+                return numerPesel[9].ToString();
+            }
+
+        }
+        public string PlecOpis
+        {
+            get
+            {
+                int plec = int.Parse(numerPesel[9].ToString());
+                if (plec % 2 == 0)
+                {
+                    return "Kobieta";
+                }
+                else
+                    return "Mężczyzna";
+            }
+        }
+        public Pesel(string numerPesel)
+        {
+            this.numerPesel = numerPesel;
+            Walidacja();
+
+        }
+
+        private void Walidacja()
+        {
+            WalidacjaPoprawnoscDlugosci();
+            WalidacjaPoprawnosciZnakow();
+            WalidacjaPoprawnosciMiesiaca();
+            WalidacjaPoprawnosciDnia();
+            //WalidacjaPoprawnosciCyfryKontrolnej();
+        }
+
+        private void WalidacjaPoprawnosciCyfryKontrolnej()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WalidacjaPoprawnosciDnia()
+        {
+            int dzien = int.Parse(numerPesel.Substring(4, 2));
+            if (dzien < 01 || dzien > 31)
+                throw new Exception("W numerze PESEL występuje niepoprawny dzień");
+
+        }
+
+        private void WalidacjaPoprawnosciMiesiaca()
+        {
+            int miesiac = int.Parse(numerPesel.Substring(2, 2));
+            int liczbaMiesiac = miesiac % 20;
+            if (liczbaMiesiac > 12)
+                throw new Exception("W numerze PESEL występuje niepoprawny miesiąc");
+        }
+
+        private void WalidacjaPoprawnosciZnakow()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                if (numerPesel[i] < '0' || numerPesel[i] > '9')
+                    throw new Exception("W numerze PESEL występują niepoprawne znaki");
+            }
+        }
+
+        private void WalidacjaPoprawnoscDlugosci()
+        {
+            int dlugosc = numerPesel.Length;
+            if (dlugosc != 11)
+                throw new Exception("Zła długość numeru PESEL");
+
+        }
+        private int Parse(string liczbaStr)
+        {
+            int wynik = 0;
+            int pozycja = 1;
+            for (int i = liczbaStr.Length - 1; i <= 0; i++)
+            {
+                wynik = ((int)liczbaStr[i] - 48) * pozycja + wynik;
+                pozycja *= 10;
+            }
+
+            return wynik;
+
+        }
+    }
+}
---
+++ PESEL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProgramPesel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pesel pesel;
+            Console.WriteLine("Wpisz numer PESEL");
+            string numerPesel = Console.ReadLine();
+            try
+            {
+                pesel = new Pesel(numerPesel);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine("Plec osoby: " + pesel.Plec);
+            Console.WriteLine("Plec osoby: " + pesel.PlecOpis);
+            Console.ReadLine();
+        }
+    }
+}
